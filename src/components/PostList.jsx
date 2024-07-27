506781651d75e9925f13b44528f53ef325986dd4
최하은 <haeun4345@gmail.com>
2024-07-27 23:59:08 +0900

    ⚡️ new post 모달창 추가

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,42 @@
+import { useState } from "react";
+
 import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+
 import classes from "./PostList.module.css";
 
 export default function PostList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
   return (
-    <ul className={classes.posts}>
-      <Post author="PostList1" body="desc" />
-      <Post author="PostList2" body="desc" />
-    </ul>
+    <>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="PostList2" body="desc" />
+      </ul>
+    </>
   );
 }
